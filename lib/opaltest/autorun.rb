de22020c385f53ed749e69a8793251ae1b6a26f5
Adam Beynon <adam@adambeynon.com>
2011-10-17 16:24:10 +0100

    make autorun handle browser conditions

--- lib/opaltest/autorun.rb
+++ lib/opaltest/autorun.rb
@@ -1,11 +1,34 @@
 require 'opaltest/unit'
 
-MiniTest::Unit.autorun
+##
+# If running in the browser then capture test output straight to the
+# page.
+
+if RUBY_ENGINE =~ /opal-browser/
+  raise "Body not loaded. Add tests into <body> element" unless $document.body
+
+  def $stdout.puts(*a)
+    a.each do |str|
+      elem = $document.createElement 'pre'
+      elem.text_content == nil ? elem.innerText = str : elem.textContent = str
+      elem.style.margin = "0px"
+      $document.body.appendChild elem
+    end
+  end # $stdout
+end
+
+##
+# If autorun is the main file (entry point), then automatically load all
+# specs from spec/
 
 if $0 == __FILE__
-  puts "in main!"
   Dir['spec/**/*.rb'].each do |spec|
     require spec
   end
 end
+
+##
+# Autorun behaviour which uses at_exit() to start tests running
+
+MiniTest::Unit.autorun
 
