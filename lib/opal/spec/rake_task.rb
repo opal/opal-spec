21f82b910c79053da2bf2dab8b3af83ebf80407a
Adam Beynon <adam.beynon@gmail.com>
2013-02-08 14:27:51 +0000

    Add rake task helper

---
+++ lib/opal/spec/rake_task.rb
@@ -0,0 +1,52 @@
+require 'opal/spec'
+
+module Opal
+  module Spec
+    class RakeTask
+      include Rake::DSL if defined? Rake::DSL
+
+      attr_accessor :name
+      attr_writer :runner_path, :url_path, :port
+
+      def initialize(name = 'opal:spec')
+        @name = name
+        yield self if block_given?
+
+        define_tasks
+      end
+
+      def define_tasks
+        desc "Run opal specs in phantomjs"
+        task @name do
+          require 'rack'
+          require 'webrick'
+
+          server = fork do
+            Rack::Server.start(:config => 'config.ru', :Port => port,
+              :Logger => WEBrick::Log.new("/dev/null"), :AccessLog => [])
+          end
+
+          system "phantomjs #{runner_path} \"#{url_path}\""
+          success = $?.success?
+
+          Process.kill(:SIGINT, server)
+          Process.wait
+
+          exit 1 unless success
+        end
+      end
+
+      def runner_path
+        @runner_path || File.join(File.dirname(__FILE__), '..', '..', '..', 'vendor', 'runner.js')
+      end
+
+      def url_path
+        @url_path || "http://localhost:9999/"
+      end
+
+      def port
+        @port || 9999
+      end
+    end
+  end
+end
